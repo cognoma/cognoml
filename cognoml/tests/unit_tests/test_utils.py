81807da5fe3fa316f04be3240ad521ae660ba3cc
Vasudevan Venkateshwaran <vasu.chemical@gmail.com>
2016-11-28 14:18:40 -0500

    PEP8 fixes
    Ensured all files meet the PEP8 standards using the pep8 script.

--- cognoml/tests/unit_tests/test_utils.py
+++ cognoml/tests/unit_tests/test_utils.py
@@ -1,6 +1,7 @@
 import os
 import mock
-from cognoml import utils 
+from cognoml import utils
+
 
 @mock.patch('os.mkdir')
 @mock.patch('os.path.exists')
@@ -8,4 +9,3 @@ def test_create_dir(mock_mkdir, mock_exists):
     mock_exists.return_value = False
     utils.create_dir('test_directory')
     mock_mkdir.assert_called_with('test_directory')
-    
