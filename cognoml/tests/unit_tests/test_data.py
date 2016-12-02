f4a4d15913c6f382af135074030e5883e7db2c67
Jesse <jesse.prestwoodtaylor@gmail.com>
2016-12-02 11:55:20 -0500

    moved figshare to data to avoid duplicate code

--- cognoml/tests/unit_tests/test_data.py
+++ cognoml/tests/unit_tests/test_data.py
@@ -1,13 +1,13 @@
-import os
 import re
 
-from cognoml import figshare
+from cognoml import data
 
 
 def test_get_article_versions():
     url = r'https://api.figshare.com/v2/articles/([0-9]*)/versions/([0-9]*$)'
     url_pattern = re.compile(url, re.MULTILINE)
-    version_to_url = figshare.get_article_versions(3487685)
+    cognoml_data = data.CognomlData()
+    version_to_url = cognoml_data.get_article_version()
     assert type(version_to_url) == dict
     for key, value in version_to_url.items():
         assert url_pattern.match(value) is not None
